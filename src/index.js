cb01c1728bba7f87eb1f1e54b625f4bd07b3a83e
cyberbadshah <kingfisher@kingfisher.rahu>
2023-06-16 16:47:02 +0530

    trial version

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(
+    <div>
+        <h1> Thank you to visit</h1>
+        <p>Work is under process</p>
+    </div>, document.getElementById("root"));
